ed92c32f4de2c30772e97c04560538aca1b0a474
hirotask <hiroto.hashi@systemi.co.jp>
2025-04-22 16:23:26 +0900

    test: remove some tests

--- test/internal.test.js
+++ test/internal.test.js
@@ -21,27 +21,7 @@ describe('edge whitespace detection', () => {
     test('${WS}H${WS}', () => {
       expect(edgeWhitespace(`${WS}H${WS}`)).toEqual({ leading: WS, leadingAscii: WS, leadingNonAscii: '', trailing: WS, trailingNonAscii: '', trailingAscii: WS });
     });
-  
-    test('${WS}\xa0${WS}HELLO${WS}WORLD${WS}\xa0${WS}', () => {
-      expect(edgeWhitespace(`${WS}\xa0${WS}HELLO${WS}WORLD${WS}\xa0${WS}`)).toEqual({ leading: WS, leadingAscii: '\xa0', leadingNonAscii: WS, trailing: WS, trailingNonAscii: '\xa0', trailingAscii: WS });
-    });
-  
-    test('\xa0${WS}HELLO${WS}WORLD${WS}\xa0', () => {
-      expect(edgeWhitespace(`\xa0${WS}HELLO${WS}WORLD${WS}\xa0`)).toEqual({ leading: '', leadingAscii: '\xa0', leadingNonAscii: WS, trailing: WS, trailingNonAscii: '\xa0', trailingAscii: '' });
-    });
-  
-    test('\xa0${WS}\xa0', () => {
-      expect(edgeWhitespace(`\xa0${WS}\xa0`)).toEqual({ leading: '', leadingAscii: '\xa0', leadingNonAscii: WS, trailing: '', trailingNonAscii: '\xa0', trailingAscii: '' });
-    });
-  
-    test('${WS}\xa0${WS}', () => {
-      expect(edgeWhitespace(`${WS}\xa0${WS}`)).toEqual({ leading: WS, leadingAscii: '\xa0', leadingNonAscii: WS, trailing: '', trailingNonAscii: '', trailingAscii: '' });
-    });
-  
-    test('${WS}\xa0', () => {
-      expect(edgeWhitespace(`${WS}\xa0`)).toEqual({ leading: WS, leadingAscii: '\xa0', leadingNonAscii: '', trailing: '', trailingNonAscii: '', trailingAscii: '' });
-    });
-  
+    
     test('HELLO WORLD', () => {
       expect(edgeWhitespace('HELLO WORLD')).toEqual({ leading: '', leadingAscii: '', leadingNonAscii: '', trailing: '', trailingNonAscii: '', trailingAscii: '' });
     });
